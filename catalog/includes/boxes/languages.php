5c35b578e906a3e7ab741dfecb8330ea4fdf6018
Laszlo Varkondi <info@vakondweb.hu>
2010-04-07 05:22:12 -0700

    don't display the flag of current language

--- catalog/includes/boxes/languages.php
+++ catalog/includes/boxes/languages.php
@@ -29,7 +29,9 @@
   $languages_string = '';
   reset($lng->catalog_languages);
   while (list($key, $value) = each($lng->catalog_languages)) {
+  if ($language != $value['directory']) {
     $languages_string .= ' <a href="' . tep_href_link(basename($PHP_SELF), tep_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . tep_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a> ';
+  }
   }
 
   $info_box_contents = array();
